e0643e89e5a2f5403d93447faa79a001803be38a
dnlgrwd87 <dgarwood@uncc.edu>
2018-10-25 12:19:47 -0700

    Add style to landing page and validation for forms

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :author, presence: true, length: { minimum: 3 }
+  validates :rating, presence: true
+  validates :text, presence: true, length: { minimum: 50 }
 end

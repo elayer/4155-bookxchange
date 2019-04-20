a0b8c2c928e8128a744359f7e5c3b32af96da687
Eric Layer <elayer@uncc.edu>
2019-04-20 19:18:45 +0000

    small changes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,12 @@
 class Book < ApplicationRecord
+    belongs_to :book
+    
+    def self.search(search)
+        if search
+            self.where(title: search)
+            self.where(author: search)
+        else Book.all
+        end
+    end
+    
 end

b3668b2fc4483972c48bbb918a4a4912278a55a4
Eric Layer <elayer@uncc.edu>
2019-04-26 00:35:02 +0000

    Review Options User Specific

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,6 +13,7 @@ class ReviewsController < ApplicationController
   
   def create
     @review = Review.new(review_params)
+    @review.user_id = current_user.id
     
     if @review.save
       redirect_to @review
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_220012) do
+ActiveRecord::Schema.define(version: 2019_04_26_001552) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 2019_04_23_220012) do
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
